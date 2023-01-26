d1ef137af3ecf61258dff94341018190175a95b9
CptBananaBoi <ryan.lippert2001@gmail.com>
2023-01-26 19:21:17 +0100

    Asteroids added in

---
+++ Assets/Scripts/GenerateAsteroidField.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateAsteroidField : MonoBehaviour
+{
+    public Transform asteroidPrefab;
+    public int FieldRadius = 100;
+    public int asteroidCount = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int loop = 0; loop < asteroidCount; loop++)
+        {
+            Transform temp = Instantiate(asteroidPrefab, Random.insideUnitSphere * FieldRadius, Random.rotation);
+            temp.localScale = temp.localScale * Random.Range(0.5f, 50f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
